2b8b8e62cc407c05f0823e6b6ea36d35bc034b17
Payal Sandeep Gatkal <payalgatkal50@gmail.com>
2026-03-11 00:46:38 +0530

    updated

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const customer=require('./models/user');
+const express = require("express");
+const mongoose = require("mongoose");
+const customer = require("./models/user");
 
-const app=express();
-const PORT=3000;
+const app = express();
+const PORT = 3000;
 
-mongoose.connect("mongodb://localhost:27017/crudapp").then(()=>{
+mongoose
+  .connect("mongodb://localhost:27017/crudapp")
+  .then(() => {
     console.log("connected to MongoDB");
-}).catch((error)=>{
+  })
+  .catch((error) => {
     console.log("Error connecting to MongoDB:", error);
-})
+  });
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("hello 1");
-})
-
-app.post('/adduser',async(req,res)=>{
-res.send("adduser");
-})
-
-app.listen(3000,()=>{
-    console.log(`server is running on port ${PORT}`)
+app.get("/", (req, res) => {
+  res.send("hello 1");
 });
 
+app.post("/adduser", async (req, res) => {
+  const newUser = await customer.create(req.body);
+  res.send(newUser);
+});
 
+app.listen(3000, () => {
+  console.log(`server is running on port ${PORT}`);
+});
