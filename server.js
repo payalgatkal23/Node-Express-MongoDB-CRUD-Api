ad26381f7ab4c6a1a9fe9bcc7dbc22e5ad8a5b72
Payal Sandeep Gatkal <payalgatkal50@gmail.com>
2026-03-10 23:24:23 +0530

    updated

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const User=require('./models/user');
 
 const app=express();
 
-mongoose.connect("")
+mongoose.connect("mongodb://localhost:27017/crudapp").then(()=>{
+    console.log("connected to MongoDB");
+}).catch((error)=>{
+    console.log("Error connecting to MongoDB:", error);
+})
+
+app.use(express.json());
+
